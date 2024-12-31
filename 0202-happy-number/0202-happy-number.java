458da6761f88bdadcafa61e0d95863bc933cf7a6
Krishna Gopal Sharma <skrishnagopal663@gmail.com>
2024-12-31 11:39:07 +0530

    Time: 1 ms (72.62%), Space: 40.8 MB (45.07%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n, fast = n;
+
+        do {
+            slow = getSumOfSquares(slow);
+            fast = getSumOfSquares(getSumOfSquares(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+
+    private int getSumOfSquares(int num) {
+        int sum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+        return sum;
+    }
+}
