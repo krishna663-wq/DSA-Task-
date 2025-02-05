07557a841dd26342ac35045bc18176666a9139d9
Krishna Gopal Sharma <skrishnagopal663@gmail.com>
2025-02-05 09:01:34 +0530

    Time: 1 ms (52.21%), Space: 41.8 MB (39.72%) - LeetHub

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        if(s1.equals(s2)) return true;
+
+        if(s1.length() != s2.length()) return false;
+
+        List<Integer> diff  = new ArrayList<>();
+        for(int i=0;i<s1.length();i++){
+            if(s1.charAt(i) != s2.charAt(i)){
+                diff.add(i);
+            }
+        }
+        if(diff.size() != 2) return false;
+
+        return s1.charAt(diff.get(0))==s2.charAt(diff.get(1)) && s1.charAt(diff.get(1)) == s2.charAt(diff.get(0));
+    }
+}
