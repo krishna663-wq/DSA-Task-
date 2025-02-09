e41b44f4a686de0d217654b3980d09e9d01bfa05
Krishna Gopal Sharma <skrishnagopal663@gmail.com>
2025-02-09 23:32:39 +0530

    Time: 42 ms (37.94%), Space: 59.5 MB (29.12%) - LeetHub

---
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+class Solution {
+    public long countBadPairs(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        long totalPairs = (long) nums.length * (nums.length - 1) / 2;
+        long goodPairs = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            int key = nums[i] - i;
+            if (map.containsKey(key)) {
+                goodPairs += map.get(key);
+            }
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+        
+        return totalPairs - goodPairs;
+    }
+}
