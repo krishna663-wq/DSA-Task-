d11bb627d8e7a78f7df91e295e78f26339d4a98b
Krishna Gopal Sharma <skrishnagopal663@gmail.com>
2025-05-18 22:18:01 +0530

    Time: 3 ms (84.18%), Space: 49.2 MB (88.29%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    private boolean isValid(int i, int j, int n, int m, char[][] grid) {
+        return i >= 0 && i < n && j >= 0 && j < m && grid[i][j] == '1';
+    }
+
+    private void numIslandsRec(int i, int j, int n, int m, char[][] grid) {
+        grid[i][j] = '0';
+
+        if (isValid(i + 1, j, n, m, grid)) {
+            numIslandsRec(i + 1, j, n, m, grid);
+        }
+        if (isValid(i - 1, j, n, m, grid)) {
+            numIslandsRec(i - 1, j, n, m, grid);
+        }
+        if (isValid(i, j + 1, n, m, grid)) {
+            numIslandsRec(i, j + 1, n, m, grid);
+        }
+        if (isValid(i, j - 1, n, m, grid)) {
+            numIslandsRec(i, j - 1, n, m, grid);
+        }
+    }
+
+    public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1') {
+                    ans++;
+                    numIslandsRec(i, j, n, m, grid);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
