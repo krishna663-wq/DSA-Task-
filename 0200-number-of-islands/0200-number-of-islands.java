97ffd1ec583c697513b731381686ae8f835b63a4
Krishna Gopal Sharma <skrishnagopal663@gmail.com>
2025-05-18 22:33:13 +0530

    Time: 3 ms (84.18%), Space: 49.4 MB (70.81%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,23 +1,22 @@
 class Solution {
 
-    private boolean isValid(int i, int j, int n, int m, char[][] grid) {
-        return i >= 0 && i < n && j >= 0 && j < m && grid[i][j] == '1';
+    private boolean isValid(int i,int j,int n,int m,char[][] grid){
+        return i>=0 && i<n && j>=0 && j<m && grid[i][j] == '1';
     }
-
-    private void numIslandsRec(int i, int j, int n, int m, char[][] grid) {
+    private void numIslandsRec(int i,int j,int n,int m,char[][] grid){
         grid[i][j] = '0';
 
-        if (isValid(i + 1, j, n, m, grid)) {
-            numIslandsRec(i + 1, j, n, m, grid);
+        if(isValid(i+1,j,n,m,grid)){
+            numIslandsRec(i+1,j,n,m,grid);
         }
-        if (isValid(i - 1, j, n, m, grid)) {
-            numIslandsRec(i - 1, j, n, m, grid);
+        if(isValid(i-1,j,n,m,grid)){
+            numIslandsRec(i-1,j,n,m,grid);
         }
-        if (isValid(i, j + 1, n, m, grid)) {
-            numIslandsRec(i, j + 1, n, m, grid);
+        if(isValid(i,j+1,n,m,grid)){
+            numIslandsRec(i,j+1,n,m,grid);
         }
-        if (isValid(i, j - 1, n, m, grid)) {
-            numIslandsRec(i, j - 1, n, m, grid);
+        if(isValid(i,j-1,n,m,grid)){
+            numIslandsRec(i,j-1,n,m,grid);
         }
     }
 
@@ -25,16 +24,14 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         int ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '1') {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
                     ans++;
-                    numIslandsRec(i, j, n, m, grid);
+                    numIslandsRec(i,j,n,m,grid);
                 }
             }
         }
-
         return ans;
     }
 }
