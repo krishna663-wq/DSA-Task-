5a8cad43580c09b89968565e8f7fb0ee1df9b3a8
Krishna Gopal Sharma <skrishnagopal663@gmail.com>
2025-05-19 22:02:27 +0530

    Time: 13 ms (15.18%), Space: 53.3 MB (81.08%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.java
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -1,12 +1,12 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
         List<Integer> data = new ArrayList<>(Collections.nCopies(n + 1, 0));
-        for(int[] x : trust){
-            data.set(x[0],data.get(x[0]) - 1);
+        for(int[] x:trust){
+            data.set(x[0],data.get(x[0]) -1);
             data.set(x[1],data.get(x[1]) + 1);
         }
         for(int i=1;i<=n;i++){
-            if(data.get(i)==n-1){
+            if(data.get(i) == n-1){
                 return i;
             }
         }
