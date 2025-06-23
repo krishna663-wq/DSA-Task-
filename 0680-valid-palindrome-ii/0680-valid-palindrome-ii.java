cd60fbb76d13b99ce35bcaf1c1a3edd889f53f5d
Krishna Gopal Sharma <skrishnagopal663@gmail.com>
2025-06-23 09:39:18 +0530

    Time: 11 ms (6.49%), Space: 45.5 MB (42.31%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -2,24 +2,31 @@ class Solution {
     public boolean validPalindrome(String s) {
         int left = 0;
         int right = s.length()-1;
-
-        while(left<right){
+        int count = 0;
+        while(left <= right){
             if(s.charAt(left) != s.charAt(right)){
-                return isPalindrome(s,left+1,right) || isPalindrome(s,left,right-1);
+                count++;
+                left++;
+            } else{
+                left++;
+                right--;
             }
-            left++;
-            right--;
         }
-        return true;
-    }
-    private boolean isPalindrome(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
+        left = 0;
+        right = s.length()-1;
+        int cnt2= 0;
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                cnt2++;
+                right--;
+            } else{
+                left++;
+                right--;
             }
-            left++;
-            right--;
         }
-        return true;
+        if(count <= 1 || cnt2 <= 1){
+            return true;
+        }
+        return false;
     }
 }
