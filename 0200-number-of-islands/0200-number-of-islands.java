5afa17d3b72e42b2eb4232e0f793b63cb74ce1f0
Krishna Gopal Sharma <skrishnagopal663@gmail.com>
2025-06-26 09:28:57 +0530

    Time: 3 ms (84.29%), Space: 49.5 MB (61.53%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,20 +1,21 @@
 class Solution {
+
     private boolean isValid(int i,int j,int p,int q,char[][] grid){
-        return i>=0 && i<p && j>=0 && j<q  && grid[i][j] == '1';
+        return i>=0 && i<p && j>=0 && j<q && grid[i][j] == '1';
     }
     private void numIslandsRec(int i,int j,int p,int q,char[][] grid){
         grid[i][j] = '0';
         if(isValid(i+1,j,p,q,grid)){
-            numIslandsRec(i+1,j,p,q,grid);        
+            numIslandsRec(i+1,j,p,q,grid);
         }
         if(isValid(i-1,j,p,q,grid)){
-            numIslandsRec(i-1,j,p,q,grid);        
+            numIslandsRec(i-1,j,p,q,grid);
         }
         if(isValid(i,j+1,p,q,grid)){
-            numIslandsRec(i,j+1,p,q,grid);        
+            numIslandsRec(i,j+1,p,q,grid);
         }
         if(isValid(i,j-1,p,q,grid)){
-            numIslandsRec(i,j-1,p,q,grid);        
+            numIslandsRec(i,j-1,p,q,grid);
         }
     }
     public int numIslands(char[][] grid) {
@@ -23,7 +24,7 @@ class Solution {
         int ans = 0;
         for(int i=0;i<p;i++){
             for(int j=0;j<q;j++){
-                if(grid[i][j]=='1'){
+                if(grid[i][j] == '1'){
                     ans++;
                     numIslandsRec(i,j,p,q,grid);
                 }
