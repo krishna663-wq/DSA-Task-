727cb10b5ab64b0fbc0b947c84f36444361b5017
Krishna Gopal Sharma <skrishnagopal663@gmail.com>
2025-06-26 22:01:05 +0530

    Time: 0 ms (100%), Space: 40.9 MB (74%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.java
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.java
@@ -5,7 +5,8 @@ class Solution {
         for(int i=0;i<arr.length;i++){
             if(arr[i]>max){
                 max = arr[i];
-            }if(i == max){
+            }
+            if(i == max){
                 chunk++;
             }
         }
