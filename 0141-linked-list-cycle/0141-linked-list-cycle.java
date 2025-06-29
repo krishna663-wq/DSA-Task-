cefa95dc523288f39afae451cb5ea0dae1e3232c
Krishna Gopal Sharma <skrishnagopal663@gmail.com>
2025-06-29 12:28:23 +0530

    Time: 0 ms (100%), Space: 44.5 MB (49.84%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -15,14 +15,13 @@ public class Solution {
         if(head == null) return false;
         ListNode slow = head;
         ListNode fast = head;
-        
+
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
-            
 
-            if(fast == slow) return true;
+            if(slow == fast) return true;
         }
-     return false;   
+        return false;
     }
 }
