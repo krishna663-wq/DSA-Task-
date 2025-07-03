feb5ad2b99353b98913e025dbeef09142c4eac51
Krishna Gopal Sharma <skrishnagopal663@gmail.com>
2025-07-03 10:31:50 +0530

    Time: 2 ms (98.06%), Space: 45.2 MB (65.45%) - LeetHub

--- 0979-di-string-match/0979-di-string-match.java
+++ 0979-di-string-match/0979-di-string-match.java
@@ -1,18 +1,19 @@
 class Solution {
     public int[] diStringMatch(String s) {
         int n = s.length();
-        int[] result = new int[n + 1];
-        int low = 0, high = n;
+        int[] res = new int[n+1];
+        int low = 0;
+        int high = n;
 
-        for (int i = 0; i < n; i++) {
-            if (s.charAt(i) == 'I') {
-                result[i] = low++;
-            } else { 
-                result[i] = high--;
+        for(int i=0;i<n;i++){
+            if(s.charAt(i) == 'I'){
+                res[i] = low++;
+            }
+            else {
+                res[i] = high--;
             }
         }
-        
-        result[n] = low; 
-        return result;
+        res[n] = low;
+        return res;
     }
 }
