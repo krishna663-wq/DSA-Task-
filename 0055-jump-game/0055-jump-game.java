b76a689eca720a2d8cce36427f3bcd6186b2c666
Krishna Gopal Sharma <skrishnagopal663@gmail.com>
2025-07-03 10:43:56 +0530

    Time: 2 ms (83.13%), Space: 46.2 MB (13.64%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -3,7 +3,7 @@ class Solution {
         int reach = 0;
         for(int i=0;i<nums.length;i++){
             if(i > reach) return false;
-            reach = Math.max(reach, i + nums[i]);
+            reach = Math.max(reach,i+nums[i]);
         }
         return true;
     }
